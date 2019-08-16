84636052921887c4d8dc32128b5fbd60800bce9c
Christopher Luc <craya1982@users.noreply.github.com>
2019-08-16 10:12:26 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_pardakhr',
-    password: '4597',
-    database: 'cs340_pardakhr'
+    host: '????',
+    user: '????',
+    password: '????',
+    database: '????'
 });
 
 module.exports = pool;
